b5f5d3c712aaa576aed47fc85e3b3519916e8a30
Renato Spakauskas <renatospaka@gmail.com>
2021-06-18 21:25:10 -0300

    style: :bug: fix person controller name
    fix person controller name (instead of patient)

--- application/controller/personController.go
+++ application/controller/personController.go
@@ -2,11 +2,12 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/renatospaka/go-clean-architecture/application/service"
-	"github.com/renatospaka/go-clean-architecture/framework/utils"
 	"github.com/renatospaka/go-clean-architecture/entity"
+	"github.com/renatospaka/go-clean-architecture/framework/utils"
 )
 
 type PersonController interface{
@@ -27,18 +28,19 @@ func NewPersonController() PersonController {
 
 func (*controller) GetPerson(resp http.ResponseWriter, req *http.Request) {
 	personService := service.NewPersonService()
-	//thisGuy := entity.Person{}
 	var thisId personId
+	//fmt.Println("GetPerson Body:", req.Body)
 
 	resp.Header().Set("Content-Type", "application/json")
-	err := json.NewDecoder(req.Body).Decode(thisId)
+	err := json.NewDecoder(req.Body).Decode(&thisId)
 	if err != nil {
 		resp.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(resp).Encode(utils.ServiceError{Message: "Error unmarshalling the person id"})
 		return 
 	}
-
-	thisGuy, err := personService.GetPerson(thisId.ID)
+	
+	fmt.Println("GetPerson thisId.ID:", thisId.ID)
+	person, err := personService.GetPerson(string(thisId.ID))
 	if err != nil {
 		resp.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(resp).Encode(utils.ServiceError{Message: "Error getting the person"})
@@ -46,9 +48,28 @@ func (*controller) GetPerson(resp http.ResponseWriter, req *http.Request) {
 	}
 
 	resp.WriteHeader(http.StatusOK)
-	json.NewEncoder(resp).Encode(thisGuy)
+	json.NewEncoder(resp).Encode(person)
 }
 
 func (*controller) AddPerson(resp http.ResponseWriter, req *http.Request) {
-	return
+	personService := service.NewPersonService()
+	var person entity.Person 
+
+	resp.Header().Set("Content-Type", "application/json")
+	err := json.NewDecoder(req.Body).Decode(&person)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(resp).Encode(utils.ServiceError{Message: "Error unmarshalling the person"})
+		return 
+	}
+
+	newPerson, err := personService.AddPerson(&person)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(resp).Encode(utils.ServiceError{Message: err.Error()})
+		return 
+	}
+
+	resp.WriteHeader(http.StatusOK)
+	json.NewEncoder(resp).Encode(newPerson)
 }
